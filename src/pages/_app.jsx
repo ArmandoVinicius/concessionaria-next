718b6c031b5e432cb0a2e7813bfeb7ae972751ce
Giovanni Alves Soares <gsoares@pop-os.localdomain>
2021-04-25 13:13:13 -0300

    feat: nprogress

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,5 +1,16 @@
-import "../styles/globals.css";
+import Router from "next/router";
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
+import "../styles/globals.scss";
 
-const App = ({ Component, pageProps }) => <Component {...pageProps} />;
+const App = ({ Component, pageProps }) => {
+  Router.events.on("routeChangeStart", () => NProgress.start());
+  Router.events.on("routeChangeComplete", () => NProgress.done());
+  Router.events.on("routeChangeError", () => NProgress.done());
+
+  return (
+    <Component {...pageProps} />
+  );
+};
 
 export default App;
