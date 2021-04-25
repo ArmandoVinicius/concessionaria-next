93d432688c4978edb3490d1f36c0bcf9ca53f625
ArmandoVinicius <armando.vinicius12@gmail.com>
2021-04-25 12:10:13 -0300

    :tada: initial commit

---
+++ src/pages/index.jsx
@@ -0,0 +1,7 @@
+const Home = () => (
+  <div className="container mx-auto flex h-screen">
+    <h1 className="text-3xl text-center m-auto font-bold">Hello World!</h1>
+  </div>
+);
+
+export default Home;
