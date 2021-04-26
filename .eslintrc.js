38fc0d50d88366b32e082ccff827f660afabfa16
ArmandoVinicius <armando.vinicius12@gmail.com>
2021-04-25 22:44:54 -0300

    feat: created card component

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,6 +17,7 @@ module.exports = {
     "react/jsx-props-no-spreading": "off",
     "react/prop-types": "off",
     "import/prefer-default-export": "off",
+    "object-curly-newline": "off",
     quotes: [2, "double", { avoidEscape: true }],
   },
 };
